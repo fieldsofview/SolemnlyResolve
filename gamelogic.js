77ac40a0f3f5423ca03a2b59656acbb6c3a85eb9
Rishabh <rishabhmakes@gmail.com>
2022-02-20 14:55:49 +0530

    front end manipulation

--- gamelogic.js
+++ gamelogic.js
@@ -1,7 +1,27 @@
-var money = 0;
-var appeal = 0;
-var transparency = 0;
-var backing = 0;
+const stats = {
+    money = 0,
+    appeal = 0,
+    transparency = 0,
+    backing = 0,
+
+    setStats: function (init = [0, 0, 0, 0]) {
+        this.money = init[0];
+        this.appeal = init[1];
+        this.transparency = init[2];
+        this.backing = init[3];
+    },
+
+    updateStats: function (delta = [0, 0, 0, 0]) {
+        this.money = this.money + delta[0];
+        this.appeal = this.appeal + delta[1];
+        this.transparency = this.transparency + delta[2];
+        this.backing = this.backing + delta[3];
+    },
+
+    getStats: function () {
+        return [this.money, this.appeal, this.transparency, this.backing];
+    },
+};
 
 var playerCoins = {
     A: 10,
@@ -41,35 +61,21 @@ console.log(playerNames);
 function scenarioSelect(number = 0) {
     switch (number) {
         case 0:
-            setStats([75, 25, 25, 50]);
+            stats.setStats([75, 25, 25, 50]);
             break;
         case 1:
-            setStats([25, 75, 25, 50]);
+            stats.setStats([25, 75, 25, 50]);
             break;
         case 2:
-            setStats([25, 50, 75, 25]);
+            stats.setStats([25, 50, 75, 25]);
             break;
         case 3:
-            setStats([50, 25, 25, 75]);
+            stats.setStats([50, 25, 25, 75]);
             break;
         default:
-            setStats([50, 50, 50, 50]);
+            stats.setStats([50, 50, 50, 50]);
             break;
     }
-}
-
-function updateStats(delta = [0, 0, 0, 0]) {
-    money = money + delta[0];
-    appeal = appeal + delta[1];
-    transparency = transparency + delta[2];
-    backing = backing + delta[3];
-}
-
-function setStats(init = [0, 0, 0, 0]) {
-    money = init[0];
-    appeal = init[1];
-    transparency = init[2];
-    backing = init[3];
 }
 
 function getRandomIntInclusive(min, max) {
@@ -102,23 +108,33 @@ function giveButton(whichOne) {
 const centralImage = document.querySelector("#u1057_img");
 centralImage.src = "/images/situations/1.png";
 
-const playerNamesBoxes = {
-    A: document.querySelector("#u890_text").children[0],
-    B: document.querySelector("#u925_text").children[0],
-    C: document.querySelector("#u938_text").children[0],
-    D: document.querySelector("#u963_text").children[0]
+const playerNamesFrontEnd = {
+    A: document.querySelector("#u890_text").children[0].children[0],
+    B: document.querySelector("#u925_text").children[0].children[0],
+    C: document.querySelector("#u938_text").children[0].children[0],
+    D: document.querySelector("#u963_text").children[0].children[0],
+
+    updatePlayerNames: function (p) {
+        this.A.textContent = p.A;
+        this.B.textContent = p.B;
+        this.C.textContent = p.C;
+        this.D.textContent = p.D;
+    },
 };
 
-const situationFrontEnd = {
-    index: document.querySelector("#u850_text").children[0],
-    progressBar : '',
-    text: document.querySelector("#u851_text").children[0],
-    choice1: document.querySelector("#u989_text").children[0],
-    choice2: document.querySelector("#u1003_text").children[0],
-    choice3: document.querySelector("#u1017_text").children[0],
+// playerNamesFrontEnd.updatePlayerNames(playerNames);
 
-    updateSituation: function(s) {
-        this.index.textContent = 'SITUATION '+ (s.index+1);
+const situationFrontEnd = {
+    index: document.querySelector("#u850_text").children[0].children[0],
+    progressBar: document.querySelector("#u853_div"),
+    text: document.querySelector("#u851_text").children[0].children[0],
+    choice1: document.querySelector("#u989_text").children[0].children[0],
+    choice2: document.querySelector("#u1003_text").children[0].children[0],
+    choice3: document.querySelector("#u1017_text").children[0].children[0],
+
+    updateSituation: function (s) {
+        this.index.textContent = 'SITUATION ' + (s.index + 1);
+        this.progressBar.style.width = (15 + 48 * s.index) + 'px';
         this.text.textContent = s.text;
         this.choice1.textContent = s.choice1;
         this.choice2.textContent = s.choice2;
@@ -126,6 +142,32 @@ const situationFrontEnd = {
     },
 };
 
+const barsFrontEnd = {
+    total: 248,
+    money: document.querySelector("#u839_div"),
+    appeal: document.querySelector("#u811_div"),
+    transparency: document.querySelector("#u827_div"),
+    backing: document.querySelector("#u791_div"),
+
+    numberToPixel: function (p) {
+        let pix = [];
+
+        p.forEach(e => {
+            pix.push((e * this.total / 100) + 'px');
+        });
+
+        return pix;
+    },
+
+    updateBars: function () {
+        let lengths = this.numberToPixel * (stats.getStats());
+        this.money.style.width = lengths[0];
+        this.appeal.style.width = lengths[1];
+        this.transparency.style.width = lengths[2];
+        this.backing.style.width = lengths[3];
+    },
+};
+// situationFrontEnd.updateSituation(situations[4]);
 
 
 // document.querySelector("#u890_text").children[0].textContent = "";
